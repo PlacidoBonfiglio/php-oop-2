b1295b9e7ac9c3305c41c03a5ad9e0804d9d8641
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-10-10 16:15:49 +0200

    added products and images

--- classes/Products.php
+++ classes/Products.php
@@ -1,5 +1,22 @@
 <?php 
 
+class Product{
+    public $image;
+    public $type;
+    public $material;
+    public $weight;
 
+    public function __construct(
+        string $_image, 
+        string $_type,
+        string $_material,
+        float $_weight
+    ){
+        $this->image = $_image;
+        $this->type = $_type;
+        $this->material = $_material;
+        $this->weight = $_weight;
+    }
+}
 
 ?>
