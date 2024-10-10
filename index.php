0cb97f82097ff851ebc81e44115e9ac39bd6d005
PlacidoBonfiglio <bonfigliodino@gmail.com>
2024-10-10 15:23:28 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!-- PHP -->
+<?php 
+require_once __DIR__ . "/classes/Products.php";
+
+?>
+
+<!-- HTML -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP OOP 2</title>
+
+    <!-- BOOTSTRAP 5.3.3 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- CSS -->
+     <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <header>
+        <h1 class="text-center fw-bold py-5">PHP OOP 2</h1>
+    </header>
+
+    <main>
+        
+    </main>
+</body>
+
+</html>
